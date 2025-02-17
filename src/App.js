275df4284c93e967525d1ee16ae6f1c25cd5f5d5
Anastasiya Kharitonova <anastasiya.kovalskaya@warecorp.com>
2025-02-17 18:20:10 +0100

    experimental list of notes (TS)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import data from "./data.json"
+import ItemsList from "./pages/ItemsList/ItemsList"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ItemsList 
+        items={data.notes}
+        renderItem={(note) => (
+          <div>
+            <h2>{note.title}</h2>
+            <ul>
+              {note.tags.map((tag, index) => (
+                <li key={index}>{tag}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      />
+    </>
   );
 }
 
