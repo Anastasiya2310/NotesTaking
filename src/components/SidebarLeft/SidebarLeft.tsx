37641bf6018ce2eb020946f27d8b1446db45235c
Anastasiya Kharitonova <anastasiya.kovalskaya@warecorp.com>
2025-03-05 17:05:48 +0100

    add gray color for active menu items (all notes, archived, tags)

--- src/components/SidebarLeft/SidebarLeft.tsx
+++ src/components/SidebarLeft/SidebarLeft.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import TagsList from "../TagsList/TagsList"
 import logo from "../../assets/images/logo.svg";
 import { ISidebarLeftProps } from "../../interfaces/interfaces";
-import { Grid2, Box, Divider, Button } from "@mui/material";
+import { Grid2, Box, Divider, Button, Typography } from "@mui/material";
 import { IconTag, IconHome, IconArchive } from "../../assets/icons";
 
 function SidebarLeft({ tags, setShowArchived, setTitle, headerTitle, setActiveTag}: ISidebarLeftProps) {
@@ -11,8 +12,11 @@ function SidebarLeft({ tags, setShowArchived, setTitle, headerTitle, setActiveTa
       setActiveTag(tag);
       setShowArchived(false);
       setTitle(`Notes Tagged: ${tag}`);
+      setActiveButton(tag);
     }
   };
+  const [activeButton, setActiveButton] = useState<string>(headerTitle.all);
+
   return (
     <Box sx={{ display: "flex", height: `calc(100vh - 10px)`, width: "272px" }}>
       <Box sx={{ display: "flex", flexDirection: "column" }}>
@@ -22,44 +26,48 @@ function SidebarLeft({ tags, setShowArchived, setTitle, headerTitle, setActiveTa
             <Box component="img" src={logo} alt="Logo" />
           </Box>
           <Button
-            onClick={() => {
+            onClick={(e) => {
               setShowArchived(false);
               setTitle(headerTitle.all);
               setActiveTag("");
+              setActiveButton(headerTitle.all);
+
             }}
             variant="text"
-            sx={{ justifyContent: "flex-start", width: "100%", px: 1.5, py: 1.25 }}
+            sx={{ backgroundColor: (activeButton === headerTitle.all) ? "neutral.100" : "transparent", justifyContent: "flex-start", width: "100%", px: 1.5, py: 1.25 }}
           >
             <IconHome sx={{ mr: 1 }} />
-            {headerTitle.all}
+            <Typography variant="h4">{headerTitle.all}</Typography>
           </Button>
           <Button
             onClick={() => {
               setShowArchived(true);
               setTitle(headerTitle.archived);
               setActiveTag("");
+              setActiveButton(headerTitle.archived);
             }}
             variant="text"
-            sx={{ justifyContent: "flex-start", width: "100%", px: 1.5, py: 1.25 }}
+            sx={{ backgroundColor: (activeButton === headerTitle.archived) ? "neutral.100" : "transparent", justifyContent: "flex-start", width: "100%", px: 1.5, py: 1.25 }}
           >
             <IconArchive sx={{ mr: 1 }} />
-            {headerTitle.archived}
+            <Typography variant="h4">{headerTitle.archived}</Typography>
           </Button>
         </Box>
 
-        {/* Sidebar Tags */}
-        <TagsList
-          items={tags}
-          renderItem={(tag) => (
-            <Grid2 container>
-              <Button
-                onClick={handleTagClick}>
-                <IconTag />
-                {tag.trim()}
-              </Button>
-            </Grid2>
-          )}
-        />
+        <Box sx={{ px: 2, py: 1.5 }}>
+          <TagsList
+            items={tags}
+            renderItem={(tag) => (
+              <Grid2 container>
+                <Button sx={{ backgroundColor: (activeButton === tag) ? "neutral.100" : "transparent", px: 1.5, py: 1.25, width: "100%", justifyContent: "flex-start" }}
+                  onClick={handleTagClick}>
+                  <IconTag sx={{ mr: 1 }}/>
+                  <Typography variant="h4">{tag.trim()}</Typography>
+                </Button>
+              </Grid2>
+            )}
+          />
+        </Box>
       </Box>
     
       <Divider orientation="vertical" flexItem />
