3ff11aeb1611409b68db515e6798d9d2cc6dd9a5
Anastasiya Kharitonova <anastasiya.kovalskaya@warecorp.com>
2025-03-19 16:14:47 +0100

    add modal for archive note (component + rebuild handling on click)

--- src/components/SidebarRight/SidebarRight.tsx
+++ src/components/SidebarRight/SidebarRight.tsx
@@ -1,27 +1,34 @@
-import { Button, Box, Typography } from "@mui/material";
-import { IconArchive, IconDelete, IconRestore }  from "../../assets/icons";
-import { SidebarRightProps, INote, INotesList } from "../../interfaces/interfaces"
+import { useState } from "react"
+import { Button, Box, Typography } from "@mui/material"
+import { IconArchive, IconDelete, IconRestore }  from "../../assets/icons"
+import { SidebarRightProps, INotesList } from "../../interfaces/interfaces"
 import axiosInstance from "../../axiosInstance"
+import Modal from "../Modal/Modal"
 
 function SidebarRight({ id, is_archived, setNotes }: SidebarRightProps){
-  const toggleIsArchived = async(id:number, currentIsArchived:boolean) => {
-    try {
-      const response = await axiosInstance.post("./notes/archive",{
-        id: id,
-        is_archived: !currentIsArchived,
-      });
-      if(response.status === 200) {
-        setNotes((prevNotes: INotesList) => {
-          return prevNotes.map((note:INote) => {
-            return note.id === id ? { ...note, is_archived: !currentIsArchived }: note
-          });
-        });
-      }
-      
-    } catch(error) {
-      console.error("Failed to update note", error);
-    }
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpen = () => {
+    setOpenModal(true);
   }
+  
+  // const toggleIsArchived = async(id:number, currentIsArchived:boolean) => {
+  //   try {
+  //     const response = await axiosInstance.post("./notes/archive",{
+  //       id: id,
+  //       is_archived: !currentIsArchived,
+  //     });
+  //     if(response.status === 200) {
+  //       setNotes((prevNotes: INotesList) => {
+  //         return prevNotes.map((note:INote) => {
+  //           return note.id === id ? { ...note, is_archived: !currentIsArchived }: note
+  //         });
+  //       });
+  //     }
+      
+  //   } catch(error) {
+  //     console.error("Failed to update note", error);
+  //   }
+  // }
 
   const deleteNote = async(id:number) => {
     try {
@@ -43,7 +50,7 @@ function SidebarRight({ id, is_archived, setNotes }: SidebarRightProps){
       <Box sx={{ pl: 2, py: 2.5, height: `calc(100vh - 130px)` }}>
         <Button variant="outlined" 
           sx={{ "justifyContent": "flex-start", width: "100%", mb: 1.5, px: 2, py: 1.5 }}
-          onClick={() => toggleIsArchived(id, is_archived)}
+          onClick={handleOpen}
         >
           {is_archived 
             ? <><IconRestore sx={{ mr: 1 }} /><Typography variant="h4">Restore Note</Typography></>
@@ -58,6 +65,7 @@ function SidebarRight({ id, is_archived, setNotes }: SidebarRightProps){
           <Typography variant="h4">Delete Note</Typography>
         </Button>
       </Box>
+      <Modal setOpenModal={setOpenModal} openModal={openModal} id={id} is_archived = {is_archived} setNotes={setNotes} />
     </Box>
   );
 }
