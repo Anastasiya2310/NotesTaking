859098938ce222e993c4a5d257a10c00cf75ccad
Anastasiya Kharitonova <anastasiya.kovalskaya@warecorp.com>
2025-03-20 10:49:09 +0100

    add unarchived toggling without modal show

--- src/components/SidebarRight/SidebarRight.tsx
+++ src/components/SidebarRight/SidebarRight.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { Button, Box, Typography } from "@mui/material"
 import { IconArchive, IconDelete, IconRestore }  from "../../assets/icons"
-import { SidebarRightProps, INotesList } from "../../interfaces/interfaces"
+import { SidebarRightProps, INotesList, INote } from "../../interfaces/interfaces"
 import axiosInstance from "../../axiosInstance"
 import Modal from "../Modal/Modal"
 
@@ -11,24 +11,24 @@ function SidebarRight({ id, is_archived, setNotes }: SidebarRightProps){
     setOpenModal(true);
   }
   
-  // const toggleIsArchived = async(id:number, currentIsArchived:boolean) => {
-  //   try {
-  //     const response = await axiosInstance.post("./notes/archive",{
-  //       id: id,
-  //       is_archived: !currentIsArchived,
-  //     });
-  //     if(response.status === 200) {
-  //       setNotes((prevNotes: INotesList) => {
-  //         return prevNotes.map((note:INote) => {
-  //           return note.id === id ? { ...note, is_archived: !currentIsArchived }: note
-  //         });
-  //       });
-  //     }
+  const toggleIsArchived = async(id:number, currentIsArchived:boolean) => {
+    try {
+      const response = await axiosInstance.post("./notes/archive",{
+        id: id,
+        is_archived: !currentIsArchived,
+      });
+      if(response.status === 200) {
+        setNotes((prevNotes: INotesList) => {
+          return prevNotes.map((note:INote) => {
+            return note.id === id ? { ...note, is_archived: !currentIsArchived }: note
+          });
+        });
+      }
       
-  //   } catch(error) {
-  //     console.error("Failed to update note", error);
-  //   }
-  // }
+    } catch(error) {
+      console.error("Failed to update note", error);
+    }
+  }
 
   const deleteNote = async(id:number) => {
     try {
@@ -50,7 +50,7 @@ function SidebarRight({ id, is_archived, setNotes }: SidebarRightProps){
       <Box sx={{ pl: 2, py: 2.5, height: `calc(100vh - 130px)` }}>
         <Button variant="outlined" 
           sx={{ "justifyContent": "flex-start", width: "100%", mb: 1.5, px: 2, py: 1.5 }}
-          onClick={handleOpen}
+          onClick={() => toggleIsArchived ? toggleIsArchived(id, is_archived) : handleOpen}
         >
           {is_archived 
             ? <><IconRestore sx={{ mr: 1 }} /><Typography variant="h4">Restore Note</Typography></>
